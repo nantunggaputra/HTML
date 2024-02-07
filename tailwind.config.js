874873ac604a3f0469db60a7378dba21710a6d39
nantunggaputra <nantunggaputra@github.com>
2024-02-08 02:40:05 +0700

    Homepage is going

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#036ca8',
+        secondary: '#0377b9',
+        third: '#025585',
+        dark: '#252525',
+      },
+    },
+  },
+  plugins: [],
+}
+
