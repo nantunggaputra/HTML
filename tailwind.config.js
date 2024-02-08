3468463a67c859dfe04eb3b54764e2127a57a859
nantunggaputra <nantunggaputra@github.com>
2024-02-08 15:50:43 +0700

    Homepage is going

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ['index.html'],
   theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
     extend: {
       colors: {
         primary: '#036ca8',
@@ -9,6 +13,9 @@ module.exports = {
         third: '#025585',
         dark: '#252525',
       },
+      screens: {
+        '2xl': '1320px'
+      }
     },
   },
   plugins: [],
