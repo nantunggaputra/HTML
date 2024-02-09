029e696a9376f664963433f2f6467616b2016de2
nantunggaputra <nantunggaputra@github.com>
2024-02-10 00:53:14 +0700

    Homepage is going

---
+++ dist/js/script.js
@@ -0,0 +1,17 @@
+//  fixed_navbar
+window.onscroll = function () {
+    const header = document.querySelector('header')
+    const fixedNav = header.offsetTop;
+
+    if (window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed');
+    } else {
+        header.classList.remove('navbar-fixed');
+    }
+};
+
+// toggle_class_active_hamburger
+const hamburger = document.querySelector('#hamburger');
+hamburger.addEventListener('click', function () {
+    hamburger.classList.toggle('hamburger-active');
+});
