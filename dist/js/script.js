528106b4b1d71bec4681922f9d0f7476ca2f7fb7
nantunggaputra <nantunggaputra@github.com>
2024-03-03 20:30:10 +0700

    Homepage is finish dark mode

--- dist/js/script.js
+++ dist/js/script.js
@@ -59,6 +59,24 @@ document.getElementById("phone").addEventListener("input", function () {
   this.value = this.value.replace(/[^0-9]/g, "");
 });
 
+// visitor_counter
+var counterContainer = document.querySelector(".website-counter");
+var resetButton = document.querySelector("#reset");
+var visitCount = localStorage.getItem("page_view");
+if (visitCount) {
+  visitCount = Number(visitCount) + 1;
+  localStorage.setItem("page_view", visitCount);
+} else {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
+}
+counterContainer.innerHTML = visitCount;
+resetButton.addEventListener("click", () => {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
+  counterContainer.innerHTML = visitCount;
+});
+
 // tobottom
 window.addEventListener('scroll', function() {
   const goToBottom = document.getElementById('tobottom');
