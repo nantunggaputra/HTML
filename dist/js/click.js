006478d679e197780702f3cdc00772eb29249f0f
nantunggaputra <nantunggaputra@github.com>
2026-02-27 00:04:04 +0700

    Homepage is finish update feature - click sound effect

--- dist/js/click.js
+++ dist/js/click.js
@@ -1,73 +1,57 @@
 // click_sound_effect
 
 class ClickSoundEffect {
-	constructor(soundPath) {
-		this.soundPath = soundPath;
-		this.volume = 0.5;
+	constructor() {
+		this.audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+		this.volume = 1;
 		this.isInitialized = false;
-		this.clickSound = null;
-		this.preloadSound();
-		this.initMobileAudio();
+		this.initUnlock();
 		this.setupClickListener();
 	}
-	preloadSound() {
-		this.clickSound = new Audio(this.soundPath);
-		this.clickSound.preload = "auto";
-		this.clickSound.volume = this.volume;
-		this.clickSound.load();
-	}
-	initMobileAudio() {
-		const unlockAudio = () => {
-			if (!this.isInitialized) {
-				const tempPlay = this.clickSound.play();
-				if (tempPlay !== undefined) {
-					tempPlay
-						.then(() => {
-							this.clickSound.pause();
-							this.clickSound.currentTime = 0;
-							this.isInitialized = true;
-						})
-						.catch((err) => {
-							console.log("Audio unlock pending:", err);
-						});
-				}
+	initUnlock() {
+		const unlock = async () => {
+			if (this.audioCtx.state === "suspended") {
+				await this.audioCtx.resume();
 			}
+			this.isInitialized = true;
 		};
-		document.addEventListener("click", unlockAudio, { once: true });
-		document.addEventListener("touchstart", unlockAudio, { once: true });
+		document.addEventListener("click", unlock, { once: true });
+		document.addEventListener("touchstart", unlock, { once: true });
 	}
 	setupClickListener() {
 		document.addEventListener("click", (e) => {
-			const isClickable = e.target.matches('button, a, [onclick], [role="button"], ' + 'input[type="submit"], input[type="button"], input[type="checkbox"], ' + 'input[type="radio"], .confetti-effect');
-			if (isClickable) {
-				this.playSound();
+			if (e.target.closest("button, a, [role='button'], input")) {
+				this.playBubbleClick();
 			}
 		});
 	}
-	playSound() {
-		if (!this.isInitialized && /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
-			return;
-		}
-		const sound = this.clickSound.cloneNode();
-		sound.volume = this.volume;
-		const playPromise = sound.play();
-		if (playPromise !== undefined) {
-			playPromise
-				.then(() => {
-					sound.addEventListener("ended", () => {
-						sound.remove();
-					});
-				})
-				.catch((err) => {
-					console.log("Sound play prevented:", err);
-				});
-		}
+	playBubbleClick() {
+		if (this.audioCtx.state !== "running") return;
+		const now = this.audioCtx.currentTime;
+		const osc = this.audioCtx.createOscillator();
+		const gain = this.audioCtx.createGain();
+		const filter = this.audioCtx.createBiquadFilter();
+		osc.type = "sine";
+		osc.frequency.setValueAtTime(500, now);
+		osc.frequency.exponentialRampToValueAtTime(300, now + 0.12);
+		gain.gain.setValueAtTime(0.001, now);
+		gain.gain.linearRampToValueAtTime(0.25, now + 0.01);
+		gain.gain.exponentialRampToValueAtTime(0.001, now + 0.18);
+		filter.type = "lowpass";
+		filter.frequency.setValueAtTime(1800, now);
+		osc.connect(filter);
+		filter.connect(gain);
+		gain.connect(this.audioCtx.destination);
+		osc.start(now);
+		osc.stop(now + 0.18);
+		osc.onended = () => {
+			osc.disconnect();
+			gain.disconnect();
+			filter.disconnect();
+		};
 	}
-	setVolume(newVolume) {
-		this.volume = Math.max(0, Math.min(1, newVolume));
-		this.clickSound.volume = this.volume;
-		console.log("Click sound volume set to:", this.volume);
+	setVolume(v) {
+		this.volume = Math.max(0, Math.min(1, v));
 	}
 }
-const clickSFX = new ClickSoundEffect("./dist/audio/click/mixkit-video-game-mystery-alert-234.mp3");
-window.clickSFX = clickSFX;
+const clickSFX = new ClickSoundEffect();
