70c1db1c8eef50943e845f120b23b1626b046a2a
Nantungga Putra <149224863+nantunggaputra@users.noreply.github.com>
2026-09-14 12:01:05 +0700

    Homepage is under development

--- netlify/functions/send-telegram.js
+++ netlify/functions/send-telegram.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
 
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Access-Control-Allow-Origin", "*");
   }
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
